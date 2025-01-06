37fcbf83a80054dccf087c755103185d34722500
willxprinxp <willianwrtwo@gmail.com>
2025-01-05 23:47:29 -0300

    Create UNION E UNION ALL.sql

---
+++ MOD 8 Joins/UNION E UNION ALL.sql
@@ -0,0 +1,37 @@
+-- UNION E UNION ALL
+
+-- Imagine que temos duas tabelas uma apenas com o gênero feminino e outra apenas como gênero masculino
+-- Quando adicionamos UNION ou UNION ALL no meio delas conseguimos a união dos dados dessas tabelas. 
+
+-- 1ª
+SELECT
+	*
+FROM
+	DimCustomer
+WHERE Gender = 'F'
+
+UNION -- REMOVE DADOS DUPLICADOS
+
+SELECT
+	*
+FROM
+	DimCustomer
+WHERE Gender = 'M'
+
+
+--2ª
+SELECT
+	FirstName,
+	BirthDate
+FROM
+	DimCustomer
+WHERE Gender = 'F'
+
+UNION ALL -- NÃO REMOVE DADOS DUPLICADOS
+
+SELECT
+	FirstName,
+	BirthDate
+FROM
+	DimCustomer
+WHERE Gender = 'M'
